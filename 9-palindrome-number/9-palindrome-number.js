3a67a1c0cf1510abf8a1aad177d9e4a6e40780ac
Paik Inbeen(Joseph) <97525377+blueline1984@users.noreply.github.com>
2022-09-27 17:36:12 +0900

    Time: 393 ms (12.00%), Space: 50.2 MB (92.38%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,20 +3,13 @@
  * @return {boolean}
  */
 const isPalindrome = function(x) {
-  const numToStr = x.toString();
-  let left = 0;
-  let right = numToStr.length - 1;
-    
-  if (numToStr.length === 2 && numToStr[0] === numToStr[1]) return true;
+  if (x < 0) return false;
 
-  for (let i = 0; i < numToStr.length / 2; i++) {
-    if (numToStr[left] === numToStr[right]) {
-      left++;
-      right--;
-    } else {
-      return false;
-    }
+  let reversedNumber = 0;
+
+  for (let i = x; i >= 1; i = Math.floor(i / 10)) {
+    reversedNumber = (i % 10) + reversedNumber * 10;
   }
-    
-  return true;
+
+  return reversedNumber === x;
 }
