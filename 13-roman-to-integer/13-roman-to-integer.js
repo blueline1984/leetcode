0547b33615b75802a8294d77bb71a982da8d5f23
Paik Inbeen(Joseph) <97525377+blueline1984@users.noreply.github.com>
2022-09-28 15:42:23 +0900

    Time: 235 ms (33.61%), Space: 47.1 MB (69.13%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -1,39 +1,29 @@
 /**
-1차 풀이
+2차 풀이
 */
 
 const romanToInt = function(s) {
   const romanNumber = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
+    M: 1000,
     D: 500,
-    M: 1000
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1
   };
-  
-  const target = {
-    CM: 900,
-    CD: 400,
-    XC: 90,
-    XL: 40,
-    IX: 9,
-    IV: 4
-  };
-  
   let value = 0;
-  let removedStr = s;
-  
-  for (let i = 0; i < Object.keys(target).length; i++) {
-    if (removedStr.includes(Object.keys(target)[i])) {
-      removedStr = removedStr.replace(Object.keys(target)[i], "");
-      value += Object.values(target)[i];
-    }
-  }
     
-  for (let i = 0; i < removedStr.length; i++) {
-    value += romanNumber[removedStr[i]];
+  for (let i = 0; i < s.length; i++) {
+    const current = romanNumber[s[i]];
+    const next = romanNumber[s[i + 1]];
+    
+    if (current < next) {
+      value += next - current;
+      i++;
+    } else {
+      value += current;  
+    }
   }
  
   return value;
